fb6813c6fecfc369e31668c5a2c4cb049de5ada5
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-09 20:41:34 -0300

    develop project

--- Projeto/pokedex/src/app/views/list/list.component.ts
+++ Projeto/pokedex/src/app/views/list/list.component.ts
@@ -1,23 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-
+  selectedPkm: any = null;
   pokemonList = [
-    {name: 'Bulbasaur', number: 1},
-    {name: 'Charmander', number: 4},
-    {name: 'Squirtle', number: 7},
-    {name: 'Pikachu', number: 15}
-  ]
-
-  constructor() {
+    { name: 'Bulbasaur', number: 1 },
+    { name: 'Charmander', number: 4 },
+    { name: 'Squirtle', number: 7 },
+    { name: 'Pikachu', number: 25 },
+  ];
+  get pkmSprite() {
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
   }
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit() {}
+
+  selectPokemon(pkm: any) {
+    this.selectedPkm = pkm;
+  }
 }
