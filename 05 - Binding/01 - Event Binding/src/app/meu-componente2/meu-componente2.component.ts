18429285aaf049916a9b495c3454330372ab4bb2
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-09 20:17:16 -0300

    event binding

---
+++ 05 - Binding/01 - Event Binding/src/app/meu-componente2/meu-componente2.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-componente2',
+  templateUrl: './meu-componente2.component.html',
+  styleUrls: ['./meu-componente2.component.css'],
+})
+export class MeuComponente2Component implements OnInit {
+  nome = 'Luis';
+  enderecoImagem = 'nome.png';
+
+  handleClick() {
+    alert('Click me');
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
