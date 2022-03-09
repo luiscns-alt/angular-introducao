f063e0e5c84ed4c2bd55a7edaeb1434eef3a667c
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-09 20:20:45 -0300

    two-way data binding manual

---
+++ 05 - Binding/02 -Two-way Data Binding manual/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyComponent } from './components/my-component.component';
+import { MeuComponente2Component } from './meu-componente2/meu-componente2.component';
+
+@NgModule({
+  declarations: [AppComponent, MyComponent, MeuComponente2Component],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
