03fb9205cfe124e8effc4deb3a2f79aa1fe275d5
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-11 20:35:56 -0300

    services

---
+++ 06 - Serviços/01 - Criando Serviços/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyComponent } from './components/my-component.component';
+import { MeuComponente2Component } from './meu-componente2/meu-componente2.component';
+import { AlunosService } from './services/alunos.service';
+
+@NgModule({
+  declarations: [AppComponent, MyComponent, MeuComponente2Component],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [AlunosService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ 06 - Serviços/01 - Criando Serviços/src/app/meu-componente2/meu-componente2.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AlunosService } from '../services/alunos.service';
+
+@Component({
+  selector: 'app-meu-componente2',
+  templateUrl: './meu-componente2.component.html',
+  styleUrls: ['./meu-componente2.component.css'],
+})
+export class MeuComponente2Component implements OnInit {
+  nome = 'TreinaWeb';
+  alunos = [];
+
+  constructor(private alunosService: AlunosService) {
+    this.alunos = this.alunosService.getAlunos();
+  }
+
+  ngOnInit() {}
+
+  handleClick() {
+    alert('Hi!');
+  }
+}
